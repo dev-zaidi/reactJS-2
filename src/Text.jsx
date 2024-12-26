a122f0fa8c4365f2e222541e20a018f48e347cb5
Hiba Areej <syedahibaareej@gmail.com>
2024-12-26 14:22:19 +0500

    Delete src directory

--- src/Text.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Text = ({...props}) => {
-  return (
-    <h3 style={{margin:'20px 30px', fontFamily:'Poppins'}}>{props.content}</h3>
-  )
-}
-
-export default Text
